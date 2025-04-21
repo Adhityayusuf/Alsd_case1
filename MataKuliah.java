6ee3de3d6053b2d994baef836aae62cc0a50fd04
FaatPrasojo <faatprasojo@gmail.com>
2025-04-21 14:18:41 +0700

    MK

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,3 +1,16 @@
 public class MataKuliah {
-    
+    String kodeMK;
+    String namaMK;
+    int sks;
+
+    public MataKuliah(String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+
+    public void tampilMatakuliah() {
+        System.out.println("Kode MK : " + kodeMK);
+        System.out.println("Nama MK : " + namaMK);
+    }
 }
